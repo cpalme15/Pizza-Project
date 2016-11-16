ba3495590d4bc90bdf9a0b4b863d5e39cdc1efbd
cpalme15 <cpalme15@emich.edu>
2016-11-16 11:23:58 -0500

    Update PizzaOrder.java

--- PizzaOrder.java
+++ PizzaOrder.java
@@ -1,14 +1,15 @@
 
 public class PizzaOrder {
 private Pizza pizza1;
-private Pizza pizza2 ;
+private Pizza pizza2;
 private Pizza pizza3;
 private int numPizzas;
 private static double totalcost;
 
 
 
-public PizzaOrder() {
+public PizzaOrder() 
+{
 	totalcost=0.0;
 	numPizzas=0;
 	
@@ -28,21 +29,20 @@ public void setNumPizzas(int numPizzas)
 {
 	if(numPizzas>1 && numPizzas<3)
 		this.numPizzas=numPizzas;
-	else
-		System.out.println("You can only order up to 3 pizzas and you must order at least 1");
 	
 }
 
-public void setPizza1(Pizza p1) {
-	this.pizza1 = p1;
+public Pizza setPizza1(Pizza p1) {
+	return pizza1 = p1;
 }
 
-public void setpizza2(Pizza p2) {
-	this.pizza2 = p2;
+public Pizza setpizza2(Pizza p2) {
+	return pizza2 =  p2;
 }
 
-public void setpizza3(Pizza p3) {
-	this.pizza3 = p3;
+public Pizza setpizza3(Pizza p3) {
+	
+	return pizza3 = p3;
 }
 public double calcTotal()
 {
